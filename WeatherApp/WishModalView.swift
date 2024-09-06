d3fb17c2693599d303bb56de01df89db8eebbc20
Ivanov Oleg <oleg-ivanov-82@mail.ru>
2024-09-06 10:14:38 +0300

    Add LabelView: UIViewRepresentable

--- WeatherApp/WishModalView.swift
+++ WeatherApp/WishModalView.swift
@@ -7,14 +7,31 @@
 
 import SwiftUI
 
+struct LabelView: UIViewRepresentable {
+    var text: String
+
+    func makeUIView(context: UIViewRepresentableContext<LabelView>) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 30)
+        label.lineBreakMode = .byWordWrapping
+        label.preferredMaxLayoutWidth = 100
+        label.textAlignment = .center
+        return label
+    }
+
+    func updateUIView(_ uiView: UILabel, context: UIViewRepresentableContext<LabelView>) {
+        uiView.text = text
+    }
+}
+
 struct WishModalView: View {
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
         VStack {
-            Text("Желаю всегда только хорошей погоды и хорошего настроения!")
-                .font(.system(.title3))
-                .bold()
+            LabelView(text: "Желаю всегда только хорошей погоды и хорошего настроения!")
                 .padding(30)
             Button("Закрыть") {
                 dismiss()
